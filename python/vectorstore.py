25ae4d3d1713b9e5f5de2afe8b176b03c84866ae
garciadavid2000 <garciadavid2003@gmail.com>
2024-09-14 21:57:23 -0400

    clean up vectorstore.py and change quiz prompt to make 5 questions instead of 3

--- python/vectorstore.py
+++ python/vectorstore.py
@@ -1,84 +1,42 @@
-import os
-# from langchain import hub
 from langchain_chroma import Chroma
-from langchain_community.document_loaders import WebBaseLoader
-from langchain_community.document_loaders import PyPDFLoader
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
-# from langchain_openai import OpenAIEmbeddings
-from pydantic import BaseModel
 from langchain_cohere import CohereEmbeddings
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_cohere.llms import Cohere
-# from langchain_openai import OpenAI
-from langchain_core.prompts import PromptTemplate
 from langchain_cohere import ChatCohere
 from langchain_core.prompts import ChatPromptTemplate
 from dotenv import load_dotenv
-from operator import itemgetter
 from langchain_groq import ChatGroq
+import chromadb
 
 # Load environment variables from .env file
 load_dotenv()
 
-# Access the API keys
-# os.environ["COHERE_API_KEY"] = getpass.getpass()
-# api_key = os.getenv('API_KEY')
-# secret_key = os.getenv('SECRET_KEY')
-
-
-# Load, chunk and index the contents of the blog.
-
-def load_pdf(pdf_name: str):
-    loader = PyPDFLoader(pdf_name)
-    docs = loader.load()
-    
-    return docs
-
-def split_files(docs):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-    splits = text_splitter.split_documents(docs)
-    return splits
-
 llm = ChatCohere(model="command-r-plus")
 # llm = ChatGroq(model="llama3-70b-8192")
 
+persistent_client = chromadb.PersistentClient(path="./chroma_data")
 
-def vector_db_store(splits):
-    vectorstore = Chroma.from_documents(documents=splits, embedding=CohereEmbeddings(model="embed-english-v3.0"))
-    return vectorstore
+vectorstore = Chroma(  
+    client=persistent_client,
+    collection_name="user_2lu5aqxzH8dTqjm1sJ0oPSTd3iN_cm12559sz0001komzb9ke7ymo",
+    embedding_function=CohereEmbeddings(model="embed-english-v3.0"),
+)
 
-def retrieve_from_vector_db(vectorstore):
-    # Retrieve and generate using the relevant snippets of the blog.
-    retriever = vectorstore.as_retriever(
-        search_type="similarity",
-        search_kwargs={"k": 5}
-    )
+retriever = vectorstore.as_retriever(
+    search_type="similarity",
+    search_kwargs={"k": 10}
+)
     
-    return retriever
-
-
-# prompt = hub.pull("rlm/rag-prompt")
-
+topic = "micriophones and the different types"    
+   
 # Open the file and read its contents into a variable
 with open('quiz_prompt.txt', 'r') as file:
     template = file.read()
-    
-topic = "Adjacency matricies and their powers."    
-   
-# llm = ChatCohere(model="command-r-plus") 
 
 prompt = ChatPromptTemplate.from_template(template)
+topic = "micriophones and the different types"    
 
-
-
-docs = load_pdf("network.pdf")
-print("docs loaded")
-splits = split_files(docs)
-print("docs split")
-vectorstore = vector_db_store(splits)
-print("vectors loaded")
-retriever = retrieve_from_vector_db(vectorstore)
+retriever = retrieve_from_vector_db()
 print("info retrieved")
 print("============================================")
 
@@ -89,9 +47,9 @@ rag_chain = (
     {"context": retriever | format_docs, "topic" : RunnablePassthrough()}
     | prompt
     | llm
-    | StrOutputParser()
+    | Json
 )
 
-topic = "Adjacency matricies and their powers"
+topic = "micriophones and the different types"   
 num_questions = 3
 print(rag_chain.invoke(topic))
