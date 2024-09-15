28cee48561177a2712cb42d22fc756df269826cb
garciadavid2000 <garciadavid2003@gmail.com>
2024-09-14 22:13:21 -0400

    Flask api for quiz generation works

--- python/vectorstore.py
+++ python/vectorstore.py
@@ -6,50 +6,45 @@ from langchain_cohere import ChatCohere
 from langchain_core.prompts import ChatPromptTemplate
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
+from langchain_core.output_parsers import JsonOutputParser
 import chromadb
 
-# Load environment variables from .env file
-load_dotenv()
+def generate_quiz(topic):
+    # Load environment variables from .env file
+    load_dotenv()
 
-llm = ChatCohere(model="command-r-plus")
-# llm = ChatGroq(model="llama3-70b-8192")
+    llm = ChatGroq(model="llama3-70b-8192")
 
-persistent_client = chromadb.PersistentClient(path="./chroma_data")
+    persistent_client = chromadb.PersistentClient(path="./chroma_data")
 
-vectorstore = Chroma(  
-    client=persistent_client,
-    collection_name="user_2lu5aqxzH8dTqjm1sJ0oPSTd3iN_cm12559sz0001komzb9ke7ymo",
-    embedding_function=CohereEmbeddings(model="embed-english-v3.0"),
-)
+    vectorstore = Chroma(  
+        client=persistent_client,
+        collection_name="user_2lu5aqxzH8dTqjm1sJ0oPSTd3iN_cm12559sz0001komzb9ke7ymo",
+        embedding_function=CohereEmbeddings(model="embed-english-v3.0"),
+    )
 
-retriever = vectorstore.as_retriever(
-    search_type="similarity",
-    search_kwargs={"k": 10}
-)
+    retriever = vectorstore.as_retriever(
+        search_type="similarity",
+        search_kwargs={"k": 10}
+    )
+        
     
-topic = "micriophones and the different types"    
-   
-# Open the file and read its contents into a variable
-with open('quiz_prompt.txt', 'r') as file:
-    template = file.read()
+    # Open the file and read its contents into a variable
+    with open('quiz_prompt.txt', 'r') as file:
+        template = file.read()
 
-prompt = ChatPromptTemplate.from_template(template)
-topic = "micriophones and the different types"    
+    prompt = ChatPromptTemplate.from_template(template)
 
-retriever = retrieve_from_vector_db()
-print("info retrieved")
-print("============================================")
 
-def format_docs(docs):
-    return "\n\n".join(doc.page_content for doc in docs)
 
-rag_chain = (
-    {"context": retriever | format_docs, "topic" : RunnablePassthrough()}
-    | prompt
-    | llm
-    | Json
-)
+    def format_docs(docs):
+        return "\n\n".join(doc.page_content for doc in docs)
 
-topic = "micriophones and the different types"   
-num_questions = 3
-print(rag_chain.invoke(topic))
+    rag_chain = (
+        {"context": retriever | format_docs, "topic" : RunnablePassthrough()}
+        | prompt
+        | llm
+        | JsonOutputParser()
+    )
+
+    return rag_chain.invoke(topic)
